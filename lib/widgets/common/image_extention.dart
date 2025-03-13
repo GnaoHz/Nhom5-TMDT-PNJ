99156d43d95db60cc60126953c2cb1fa68304ce4
nguyen dao <nguyendao21012002@gmail.com>
2025-03-13 10:28:32 +0700

    update common

--- lib/widgets/common/image_extention.dart
+++ lib/widgets/common/image_extention.dart
@@ -9,6 +9,27 @@ class ImageAsset{
   static String banner1 = '$baseImagePathPNG/banner1.png';
   static String banner2 = '$baseImagePathPNG/banner2.png';
   static String banner3 = '$baseImagePathPNG/banner3.png';
+  static String homeVongLac = '$baseImagePathPNG/home_vong_lac.png';
+  static String homeNhanCuoi = '$baseImagePathPNG/home_nhan_cuoi.png';
+  static String homeNhanCauHon = '$baseImagePathPNG/home_nhan_cau_hon.png';
+  static String homeNhan = '$baseImagePathPNG/home_nhan.png';
+  static String homeKieng = '$baseImagePathPNG/home_kieng.png';
+  static String homeDayChuyen = '$baseImagePathPNG/home_day_chuyen.png';
+  static String homeBongTai = '$baseImagePathPNG/home_bong_tai.png';
+  static String homeNhanCap = '$baseImagePathPNG/home_nhan_cap.png';
+  static String columnLogoApp = '$baseImagePathPNG/column_logo_app.png';
+  static String social = '$baseImagePathPNG/social.png';
+  static String pay = '$baseImagePathPNG/pay.png';
+  static String zalo = '$baseImagePathPNG/zalo.png';
+  static String callPhoneNumber = '$baseImagePathPNG/call_phone_number.png';
+  static String users = '$baseImagePathPNG/users.png';
+  static String downArrow = '$baseImagePathPNG/down-arrow.png';
+  static String money = '$baseImagePathPNG/money.png';
+  static String stripe = '$baseImagePathPNG/stripe.png';
+  static String remove = '$baseImagePathPNG/remove.png';
+  static String check = '$baseImagePathPNG/check.png';
+  static String logout = '$baseImagePathPNG/logout.png';
+  static String purchased = '$baseImagePathPNG/purchased.png';
 
 
   // SVG
@@ -23,4 +44,16 @@ class ImageAsset{
   static String buyCartUn = '$baseImagesPathSVG/buy_cart_un$format';
   static String user = '$baseImagesPathSVG/user$format';
   static String userUn = '$baseImagesPathSVG/user_un$format';
+  static String filter = '$baseImagesPathSVG/filter$format';
+  static String horizontal = '$baseImagesPathSVG/horizontal$format';
+  static String backButton = '$baseImagesPathSVG/back_button$format';
+  static String more = '$baseImagesPathSVG/more$format';
+  static String vnpay = '$baseImagesPathSVG/vnpay$format';
+  static String arrowRight = '$baseImagesPathSVG/arrowRight$format';
+  static String personalInfo = '$baseImagesPathSVG/personalInfo$format';
+  static String GPS = '$baseImagesPathSVG/GPS$format';
+  static String cartPlus = '$baseImagesPathSVG/cartPlus$format';
+  static String favourist = '$baseImagesPathSVG/favourist$format';
+  static String notification = '$baseImagesPathSVG/notification$format';
+  // static String arrowRight = '$baseImagesPathSVG/arrowRight$format';
 }
